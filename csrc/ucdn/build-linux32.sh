c1b55149a4110d8b26f5bdb73e30e1d5e97de2ca
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-01-23 00:53:02 +0200

    removed CFLAGS/CXXFLAGS

--- csrc/ucdn/build-linux32.sh
+++ csrc/ucdn/build-linux32.sh
@@ -1,4 +1,4 @@
 #TODO: remove the MINGW32 hack
-gcc $CFLAGS ucdn.c -shared -o ../../bin/linux32/libucdn.so -D__MINGW32__
+gcc -O2 -s -static-libgcc ucdn.c -shared -o ../../bin/linux32/libucdn.so -D__MINGW32__
 
 #cd ../.. && linux/bin/luajit ucdn.lua
--- csrc/ucdn/build-mingw32.sh
+++ csrc/ucdn/build-mingw32.sh
@@ -1,3 +1,3 @@
-gcc $CFLAGS ucdn.c -shared -o ../../bin/mingw32/ucdn.dll
+gcc -O2 -s -static-libgcc ucdn.c -shared -o ../../bin/mingw32/ucdn.dll
 
 #cd ../.. && bin/mingw32/luajit ucdn.lua
