4932397640f442e734fb54f8a6621ad62a14292c
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-26 20:09:39 +0200

    rebuild for osx with @loader_path-based install_name

--- csrc/ucdn/build-osx32.sh
+++ csrc/ucdn/build-osx32.sh
@@ -1,2 +1,2 @@
 #TODO: remove the MINGW32 hack
-gcc -arch i386 -O2 ucdn.c -shared -o ../../bin/osx32/libucdn.dylib -D__MINGW32__
+gcc -arch i386 -O2 ucdn.c -shared -install_name @loader_path/libucdn.dylib -o ../../bin/osx32/libucdn.dylib -D__MINGW32__
--- csrc/ucdn/build-osx64.sh
+++ csrc/ucdn/build-osx64.sh
@@ -1,2 +1,2 @@
 #TODO: remove the MINGW32 hack
-gcc -arch x86_64 -O2 ucdn.c -shared -o ../../bin/osx64/libucdn.dylib -D__MINGW32__
+gcc -arch x86_64 -O2 ucdn.c -shared -install_name @loader_path/libucdn.dylib -o ../../bin/osx64/libucdn.dylib -D__MINGW32__
